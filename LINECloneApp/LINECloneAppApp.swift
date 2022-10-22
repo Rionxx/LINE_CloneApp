97c78331cc8e78385df6c4d0683c6ff10729d212
Rion <rionfx.42@gmail.com>
2022-10-22 18:30:36 +0900

    first commit

---
+++ LINECloneApp/LINECloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LINECloneAppApp.swift
+//  LINECloneApp
+//
+//  Created by FX on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LINECloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# LINEClone App
